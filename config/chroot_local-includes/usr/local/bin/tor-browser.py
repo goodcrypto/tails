b65d2eac2fc62707d2202aa68bd3d9955df4a994
GoodCrypto <support@goodcrypto.com>
2016-06-14 18:23:18 +0000

    Updated for compatibility with Tails2.4rc1

--- config/chroot_local-includes/usr/local/bin/tor-browser.py
+++ config/chroot_local-includes/usr/local/bin/tor-browser.py
@@ -1,11 +1,11 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 """
     Set up the environment and then start the tor browser.
 
-    Conversion from bash to python by goodcrypto.com
-"""
-from __future__ import print_function
+    Test with "python3 tor-browser.py doctest".
 
+    goodcrypto.com converted from bash to python and added basic tests.
+"""
 import os
 import sys
 from gettext import gettext
@@ -23,7 +23,7 @@ os.environ['PATH'] = '/usr/local/bin:/usr/bin:/bin'
 
 PROFILE = '{}/.tor-browser/profile.default'.format(os.environ['HOME'])
 
-def main(*args):
+def main(args):
     """
         >>> main([])
     """
@@ -47,7 +47,7 @@ def main(*args):
             os.environ['TOR_SOCKS_HOST'] = '127.0.0.1'
             os.environ['TOR_SOCKS_PORT'] = '9150'
 
-            start_browser(*args)
+            start_browser(args)
 
     except sh.ErrorReturnCode as error:
         print(error.stderr, file=sys.stderr)
@@ -60,32 +60,33 @@ def ask_for_confirmation():
         >>> ask_for_confirmation()
         False
     """
+
     # Skip dialog if user is already running Tor Browser:
     try:
         sh.pgrep('-u', 'amnesia', '-f', os.path.join(TBB_INSTALL, 'firefox'))
-        return False
+        result = False
     except sh.ErrorReturnCode_1:
         dialog_title = gettext('Tor is not ready')
         dialog_text = gettext('Tor is not ready. Start Tor Browser anyway?')
         dialog_start = gettext('Start Tor Browser')
         dialog_cancel = gettext('Cancel')
-        # zenity can't set the default button to cancel, so we switch the
-        # labels and interpret the return value as its negation.
-        sh_results = sh.zenity('--question',
-                               '--title="{}"'.format(dialog_title),
-                               '--text="{}"'.format(dialog_text),
-                               '--cancel-label="{}"'.format(dialog_start),
-                               '--ok-label="{}"'.format(dialog_cancel),
-                               _ok_code=[0, 1])
-        return sh_results.exit_code == 1
 
-def start_browser(*args):
+        # results 0 == True; 1 == False; 5 == Timeout
+        results = sh.zenity('--question', '--title', "{}".format(dialog_title),
+            '--default-cancel', '--ok-label', '{}'.format(dialog_start),
+            '--cancel-label', '{}'.format(dialog_cancel),
+            '--text', '{}'.format(dialog_text), _ok_code=[0,1,5])
+        result = results.exit_code == 0
+
+    return result
+
+def start_browser(args):
     """
         Start the browser.
 
         >>> start_browser([])
     """
-    if not os.path.exists(PROFILE):
+    if not os.path.exists(PROFILE) or not os.path.isdir(PROFILE):
         run = sh.Command('/usr/local/lib/generate-tor-browser-profile')
         run()
 
@@ -95,7 +96,7 @@ def start_browser(*args):
     os.environ['TMPDIR'] = tmp_dir
 
     # We need to set general.useragent.locale properly to get
-    # localized search plugilns (and perhaps other things too). It is
+    # localized search plugins (and perhaps other things too). It is
     # not enough to simply set intl.locale.matchOS to true.
     config_best_tor_browser_locale(PROFILE)
 
@@ -103,15 +104,21 @@ def start_browser(*args):
     if len(args) > 0:
         full_args = full_args + list(args)
 
-    exec_firefox(*full_args)
+    exec_firefox(full_args)
 
+'''
+    >>> # run script
+    >>> this_command = sh.Command(sys.argv[0])
+    >>> this_command()
+    <BLANKLINE>
+'''
 if __name__ == '__main__':
     if sys.argv and len(sys.argv) > 1:
         if sys.argv[1] == 'doctest':
             from doctest import testmod
             testmod()
         else:
-            main(sys.argv[1:])
+            main(sys.argv)
     else:
         main([])
 
